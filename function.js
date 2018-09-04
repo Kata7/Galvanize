edf1717d0ad103de1e5a2701b1621186c64dfe63
Kata7 <40526277+Kata7@users.noreply.github.com>
2018-09-04 15:58:14 -0600

    Delete function.js

--- function.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".toggle").click(function(){
-    	$(this).html() === 'hide' ? $(this).html('show') : $(this).html('hide');
-        $(this).parent().parent().parent().find('.box').toggle();
-    });
-});
